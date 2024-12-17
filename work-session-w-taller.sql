e311ff3dfb62535d1e9a758afb82ea163df644f1
eduardollopes <27728805+eduardollopes@users.noreply.github.com>
2024-12-17 16:39:59 -0300

    Firts commit

---
+++ work-session-w-taller.sql
@@ -0,0 +1,4 @@
+We are given a table consisting of two columns, Name, and Profession. We need to query all the names immediately followed by the first letter in the profession column enclosed in parenthesis.
+Name Profession
+Eduardo Data Engineer
+Eduardo (D)
