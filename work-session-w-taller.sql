92eb946da753e5c725f7a22767b844ce9e60232d
eduardollopes <27728805+eduardollopes@users.noreply.github.com>
2024-12-17 16:41:04 -0300

    Add files via upload

--- work-session-w-taller.sql
+++ work-session-w-taller.sql
@@ -1,4 +1,16 @@
-We are given a table consisting of two columns, Name, and Profession. We need to query all the names immediately followed by the first letter in the profession column enclosed in parenthesis.
-Name Profession
-Eduardo Data Engineer
-Eduardo (D)
+-- We are given a table consisting of two columns, Name, and Profession. 
+-- We need to query all the names immediately followed by the first letter in the profession column enclosed in parenthesis.
+-- Name Profession
+-- Eduardo Data Engineer
+-- Eduardo (D)
+
+with cte_prof as (
+SELECT 
+  name,
+  substring(profession, 1) as first_letter_profession
+FROM profession 
+)
+select 
+  name
+  concat("(",first_letter_profession,")") as profession_abbreviation
+from cte_prof
